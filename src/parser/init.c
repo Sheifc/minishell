7d2ae2a2d37ac0182852c0544d91fc13a1d8ca22
SFC <115345487+Sheifc@users.noreply.github.com>
2024-07-30 12:50:03 +0200

    signals example

--- src/parser/init.c
+++ src/parser/init.c
@@ -38,3 +38,59 @@ void    init(t_shell *data, char **envp)
     init_struct(data, envp);
     init_env(data, envp);
 }
+
+/* 
+extern int	g_sig;
+
+void	heredoc_handler(int signum)
+{
+	(void)signum;
+	exit(130);
+}
+
+void	signal_d(t_shell *data)
+{
+	free_data(data);
+	free_env_node(&data->env);
+	free_env_node(&data->export);
+	printf("exit\n");
+	exit(data->status);
+}
+
+void	signal_c(int signal)
+{
+	(void)signal;
+	if (g_sig == 1)
+	{
+		write(1, "\033[K\n", 5);
+		rl_replace_line("", 0);
+		rl_redisplay();
+	}
+	else if (g_sig == 0)
+	{
+		rl_on_new_line();
+		rl_replace_line("", 0);
+		rl_redisplay();
+		write(1, "\033[K\n", 5);
+		rl_on_new_line();
+		rl_replace_line("", 0);
+		rl_redisplay();
+	}
+}
+
+void	signal_quit(int signal)
+{
+	(void)signal;
+	write(2, "Quit (core dumped)\n", 20);
+}
+
+void	init_signal(void)
+{
+	signal(SIGTSTP, SIG_IGN);
+	if (g_sig == 0)
+		signal(SIGQUIT, SIG_IGN);
+	else
+		signal(SIGQUIT, signal_quit);
+	signal(SIGINT, signal_c);
+}
+*/
