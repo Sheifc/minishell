e393625711f25699f0ef458411fdab09ab5684a0
SFC <115345487+Sheifc@users.noreply.github.com>
2024-08-23 11:55:57 +0200

    Path refactored for executor

--- src/executor/path.c
+++ src/executor/path.c
@@ -9,32 +9,41 @@ static void	access_cmd(t_shell *data, t_cmd *cmd)
 	}
 }
 
+static int	find_exec_path(char **content_splited, t_shell *data, t_cmd *cmd, int i)
+{
+	char	*path_temp;
+	char	*path;
+
+	path_temp = ft_strjoin(content_splited[i], "/");
+	path = ft_strjoin(path_temp, cmd->arg[0]);
+	free(path_temp);
+	if (access(path, X_OK) == 0)
+	{
+		data->path = path;
+		return (1);
+	}
+	free(path);
+	return (0);
+}
+
 void	get_path(t_shell *data, t_cmd *cmd)
 {
 	char	**content_splited;
 	int		i;
-	char	*path;
-	char	*path_temp;
 	char	*content;
 
 	i = -1;
 	access_cmd(data, cmd);
+	if (data->path)
+		return ;
 	content = get_value(data->env, "PATH");
 	if (!content)
 		return ;
 	content_splited = ft_split(content, ':');
 	while (content_splited[++i] != NULL)
 	{
-		path_temp = ft_strjoin(content_splited[i], "/");
-		path = ft_strjoin(path_temp, cmd->arg[0]);
-		free(path_temp);
-		if (access(path, X_OK) == 0)
-		{
-			data->path = path;
-			ft_free_matrix(content_splited);
-			return ;
-		}
-		free(path);
+		if (find_exec_path(content_splited, data, cmd, i))
+			break ;
 	}
 	ft_free_matrix(content_splited);
 	return ;
