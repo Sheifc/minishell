6c55f65e1405b545eef553acbcefa8c29c6c221a
SFC <115345487+Sheifc@users.noreply.github.com>
2024-08-23 10:57:46 +0200

    Fix echo to accept arguments like -nd -n hola

--- src/executor/echo.c
+++ src/executor/echo.c
@@ -16,26 +16,20 @@ static void	print_cmd(t_cmd *cmd, int i, int newline)
 void	ft_echo(t_shell *data, t_cmd *cmd)
 {
 	int	i;
-	int	j;
+	int j;
 	int	newline;
 
 	i = 1;
-	j = 1;
 	newline = 1;
-	while (cmd->arg[i] && cmd->arg[i][0] == '-')
+	while (cmd->arg[i] && ft_strncmp(cmd->arg[i], "-n", 2) == 0)
 	{
-		i++;
-		while (cmd->arg[1][j] == 'n')
-		{
-			newline = 0;
+		j = 1;
+		while (cmd->arg[i][j] == 'n')
 			j++;
-		}
-		if (cmd->arg[1][j] != '\0')
-		{
-			newline = 1;
-			i--;
-			break ;
-		}
+		if (cmd->arg[i][j] != '\0')
+			break;
+		newline = 0;
+		i++;
 	}
 	print_cmd(cmd, i, newline);
 	data->status = 0;
